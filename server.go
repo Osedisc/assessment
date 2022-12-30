7a9deea4f8ee456008cad8be7cedf86d94044881
Osedisc <harirak.disc@gmail.com>
2022-12-30 13:54:55 +0700

    init function main

--- server.go
+++ server.go
@@ -1,12 +1,52 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			auth := c.Request().Header.Get("Authorization")
+			if auth != "November 10, 2009" {
+				return c.NoContent(http.StatusUnauthorized)
+			}
+			return next(c)
+		}
+	})
+
+	e.POST("/expenses", handler.PostExpenses)
+	e.GET("/expenses/:id", handler.GetExpensebyid)
+	e.PUT("/expenses/:id", handler.UpdateExpense)
+	e.GET("/expenses", handler.GetAllExpenses)
+
 	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	fmt.Println("start at port:", e.Start(os.Getenv("PORT")))
+
+	//Graceful shutdown
+	go func() {
+		if err := e.Start(os.Getenv("PORT")); err != nil && err != http.ErrServerClosed { // Start server
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
